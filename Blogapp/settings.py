cf154a7433e4a36c6449307e0cd0def2c3059c92
tushar07059 <tushar24bhandari@gmail.com>
2026-03-29 17:33:30 +0530

    Fixed static files issue

--- Blogapp/settings.py
+++ Blogapp/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -118,8 +119,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS=[BASE_DIR/"static"]
+import os
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 MEDIA_URL='/media/'
 MEDIA_ROOT= BASE_DIR/'media'
 
