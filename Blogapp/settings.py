b06eb809d6248af6d71626d3273b0de8a641ec37
tushar07059 <tushar24bhandari@gmail.com>
2026-03-29 16:59:57 +0530

    added Procfile

--- Blogapp/settings.py
+++ Blogapp/settings.py
@@ -26,8 +26,7 @@ SECRET_KEY = 'django-insecure-(qrz6^=d(k%gx0w&fk_e8%oata@xfq#_5r(dpvw3xvi1hh)65t
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -119,6 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS=[BASE_DIR/"static"]
 MEDIA_URL='/media/'
 MEDIA_ROOT= BASE_DIR/'media'
